8aafbaa5c7eb64c72f4fe638a2b515cbba55cb44
Naoking <naokiyokota.126@gmail.com>
2021-12-05 00:29:35 +0900

    Part1 実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '39789c48070e29294fc3c964b27e6940e83d38896ed30903f35c5934316edf884b14eba3841a0a99a7e8499e6b895a416e7b3f9c734c09ee69b2ea2218be5f8c'
